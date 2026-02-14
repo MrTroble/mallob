3f64a795decaad7357319a7e03423ab88ad6173b
MrTroble <nifroh56@gmail.com>
2026-02-14 15:02:14 +0100

    init: basic files and cmake presets

---
+++ src/test/test_mgi_base.cpp
@@ -0,0 +1,31 @@
+#include "mpi.h"
+#include "util/random.hpp"
+#include "util/logger.hpp"
+#include "util/sys/timer.hpp"
+#include "comm/mympi.hpp"
+#include "util/params.hpp"
+#include "util/sys/process.hpp"
+
+void testRoutine() {
+    
+}
+
+int main(int argc, char* argv[]) {
+
+    MyMpi::init();
+    Timer::init();
+    int rank = MyMpi::rank(MPI_COMM_WORLD);
+
+    Process::init(rank);
+
+    Random::init(rand(), rand());
+    Logger::init(rank, V5_DEBG);
+
+    Parameters params;
+    params.init(argc, argv);
+    MyMpi::setOptions(params);
+
+    testRoutine();
+
+    MPI_Finalize();
+}
