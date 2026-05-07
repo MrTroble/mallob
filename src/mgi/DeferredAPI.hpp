439c4bbaaac5dbfe3c2bc8e153ac9fa3e0ebd31c
MrTroble <nifroh56@gmail.com>
2026-05-07 13:09:36 +0200

    fix: typos

--- src/mgi/DeferredAPI.hpp
+++ src/mgi/DeferredAPI.hpp
@@ -58,7 +58,7 @@ namespace mgi
             for (auto i = first; i != last; i++)
             {
                 const auto eval = map.insert(*i);
-al                 assert(eval.second);
+                assert(eval.second);
             }
             #else
             map.insert(first, last);
