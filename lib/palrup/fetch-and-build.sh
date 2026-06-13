58e45d91b89795c34544755bebb8a8ddc9e7655f
MrTroble <nifroh56@gmail.com>
2026-06-13 12:10:02 +0200

    deps: try to remove strict prototypes because of Werror deps chain ... to bad

--- lib/palrup/fetch-and-build.sh
+++ lib/palrup/fetch-and-build.sh
@@ -6,7 +6,7 @@ dirname="PalRUP-Check"
 branchorcommit="31f52fd70d39c1a6f7eeb3ce1bb7d97dbee6142d" # updated 2026-01-29
 fetch_and_extract $dirname CMakeLists.txt https://github.com/rubenGoetz/PalRUP-Check/archive/${branchorcommit}.zip
 
-sed -i 's/-Werror//g' CMakeLists.txt
+sed -i 's/-Werror/-Wno-strict-prototypes/g' CMakeLists.txt
 
 echo "[$dirname] Building ..."
 mkdir -p build
